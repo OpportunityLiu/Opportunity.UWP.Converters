2425ee3492f221d5d5131d770819653928d3731d
OpportunityLiu <liuzhengyuan13@outlook.com>
2017-05-28 02:25:05 +0800

    NullTestConverter

---
+++ Opportunity.Converters/+Chains/NullTestConverter.cs
@@ -0,0 +1,47 @@
+﻿using Opportunity.Converters.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace Opportunity.Converters
+{
+    /// <summary>
+    /// Test a value is <c>null</c> or not, returns a <see cref="bool"/> as result.
+    /// </summary>
+    public class NullTestConverter : ChainConverter
+    {
+        /// <summary>
+        /// Return <see cref="bool"/> when the value is <c>null</c>.
+        /// </summary>
+        public bool IfNull
+        {
+            get => (bool)GetValue(IfNullProperty); set => SetValue(IfNullProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="IfNull"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty IfNullProperty =
+            DependencyProperty.Register("IfNull", typeof(bool), typeof(NullTestConverter), new PropertyMetadata(true));
+
+        /// <inheritdoc />
+        protected override object ConvertBackImpl(object value, object parameter, string language)
+        {
+            var v = ConvertHelper.ChangeType<bool>(value);
+            if (v == IfNull)
+                return null;
+            return new object();
+        }
+
+        /// <inheritdoc />
+        protected override object ConvertImpl(object value, object parameter, string language)
+        {
+            if (value == null)
+                return IfNull;
+            return !IfNull;
+        }
+    }
+}
--- Opportunity.Converters/Properties/AssemblyInfo.cs
+++ Opportunity.Converters/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Runtime.InteropServices;
 
 internal class AssemblyInfo
 {
-    public const string Version = "1.0.15.0";
+    public const string Version = "1.0.16.0";
 }
