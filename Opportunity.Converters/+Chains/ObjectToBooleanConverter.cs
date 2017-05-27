dcbc38753e069d573cb742c1d04dea435a4262b1
OpportunityLiu <liuzhengyuan13@outlook.com>
2017-05-28 00:10:35 +0800

    change ObjectToBooleanConverter to accept multi objects.

--- Opportunity.Converters/+Chains/ObjectToBooleanConverter.cs
+++ Opportunity.Converters/+Chains/ObjectToBooleanConverter.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using static Opportunity.Converters.Internal.ConvertHelper;
@@ -14,36 +16,169 @@ namespace Opportunity.Converters
     [Windows.UI.Xaml.Markup.ContentProperty(Name = nameof(NextConverter))]
     public sealed class ObjectToBooleanConverter : ChainConverter
     {
+        private class ValueCollection : IList
+        {
+            public ValueCollection(ObjectToBooleanConverter parent)
+            {
+                this.parent = parent;
+            }
+
+            public readonly List<object> Items = new List<object>();
+            private readonly ObjectToBooleanConverter parent;
+
+            public object this[int index] { get => Items[index]; set => Items[index] = value; }
+
+            bool IList.IsFixedSize => ((IList)this.Items).IsFixedSize;
+
+            bool IList.IsReadOnly => ((IList)this.Items).IsReadOnly;
+
+            public int Count => this.Items.Count;
+
+            bool ICollection.IsSynchronized => ((IList)this.Items).IsSynchronized;
+
+            object ICollection.SyncRoot => ((IList)this.Items).SyncRoot;
+
+            public int Add(object value)
+            {
+                var r = ((IList)this.Items).Add(value);
+                this.parent.refresh();
+                return r;
+            }
+
+            public void Clear()
+            {
+                this.Items.Clear();
+                this.parent.refresh();
+            }
+
+            public bool Contains(object value)
+            {
+                var comparer = this.parent.ValueComparer;
+                if (comparer == null)
+                    return this.Items.Contains(value);
+                foreach (var item in this.Items)
+                {
+                    if (comparer.Equals(value, item))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            public void CopyTo(Array array, int index)
+            {
+                ((IList)this.Items).CopyTo(array, index);
+            }
+
+            public IEnumerator GetEnumerator()
+            {
+                return ((IList)this.Items).GetEnumerator();
+            }
+
+            public int IndexOf(object value)
+            {
+                return ((IList)this.Items).IndexOf(value);
+            }
+
+            public void Insert(int index, object value)
+            {
+                this.Items.Insert(index, value);
+                this.parent.refresh();
+            }
+
+            public void Remove(object value)
+            {
+                var c = this.parent.ValueComparer;
+                if (c == null)
+                {
+                    if (this.Items.Remove(value))
+                        this.parent.refresh();
+                    return;
+                }
+                var index = this.Items.FindIndex(o => c.Equals(o, value));
+                if (index != -1)
+                {
+                    this.Items.RemoveAt(index);
+                    this.parent.refresh();
+                }
+            }
+
+            public void RemoveAt(int index)
+            {
+                this.Items.RemoveAt(index);
+                this.parent.refresh();
+            }
+        }
+
+        private static Type merge(Type left, Type right)
+        {
+            if (left == null && right == null)
+                return typeof(object);
+            if (left == null)
+            {
+                if (right == null)
+                    return typeof(object);
+                else
+                    return right;
+            }
+            if (right == null)
+                return left;
+            if (left == right)
+                return left;
+            // UNDONE: left 和 right 的共同基类
+            return typeof(object);
+        }
+
+        private void refresh()
+        {
+            var list = default(IEnumerable<object>);
+            if (this.valuesForTrue != null)
+            {
+                if (this.valuesForFalse == null)
+                    list = this.valuesForTrue.Cast<object>();
+                else
+                    list = this.valuesForTrue.Cast<object>().Concat(this.valuesForFalse.Cast<object>());
+            }
+            if (this.valuesForFalse != null)
+            {
+                list = this.valuesForFalse.Cast<object>();
+            }
+            if (list == null)
+                list = Enumerable.Empty<object>();
+            this.valueType = list.Aggregate(typeof(object), (t, o) => merge(t, o?.GetType()));
+        }
+
+        private Type valueType = typeof(object);
+
+        private ValueCollection valuesForTrue;
         /// <summary>
         /// <see cref="object"/>s will be converted to <c>true</c>.
         /// </summary>
-        public object ValueForTrue
-        {
-            get => GetValue(ValueForTrueProperty); set => SetValue(ValueForTrueProperty, value);
-        }
+        public IList ValuesForTrue => LazyInitializer.EnsureInitialized(ref this.valuesForTrue, () => new ValueCollection(this));
 
-        /// <summary>
-        /// Identifies the <see cref="ValueForTrue"/> dependency property.
-        /// </summary>
-        public static readonly DependencyProperty ValueForTrueProperty =
-            DependencyProperty.Register("ValueForTrue", typeof(object), typeof(ObjectToBooleanConverter), new PropertyMetadata(null, ValueChangedCallback));
-
+        private ValueCollection valuesForFalse;
         /// <summary>
         /// <see cref="object"/>s will be converted to <c>false</c>.
         /// </summary>
-        public object ValueForFalse
+        public IList ValuesForFalse => LazyInitializer.EnsureInitialized(ref this.valuesForFalse, () => new ValueCollection(this));
+
+        /// <summary>
+        /// The <see cref="IEqualityComparer"/> used to compare values.
+        /// </summary>
+        public IEqualityComparer ValueComparer
         {
-            get => GetValue(ValueForFalseProperty); set => SetValue(ValueForFalseProperty, value);
+            get => (IEqualityComparer)GetValue(ValueComparerProperty); set => SetValue(ValueComparerProperty, value);
         }
 
         /// <summary>
-        /// Identifies the <see cref="ValueForFalse"/> dependency property.
+        /// Identifies the <see cref="ValueComparer"/> dependency property.
         /// </summary>
-        public static readonly DependencyProperty ValueForFalseProperty =
-            DependencyProperty.Register("ValueForFalse", typeof(object), typeof(ObjectToBooleanConverter), new PropertyMetadata(null, ValueChangedCallback));
+        public static readonly DependencyProperty ValueComparerProperty =
+            DependencyProperty.Register("ValueComparer", typeof(IEqualityComparer), typeof(ObjectToBooleanConverter), new PropertyMetadata(null));
 
         /// <summary>
-        /// Returns when <c>value != ValueForTrue &amp;&amp; value != ValueForFalse</c>.
+        /// Returns when <c>value</c> is in neither <see cref="ValuesForTrue"/> nor <see cref="ValuesForFalse"/>.
         /// </summary>
         public bool IfNeither
         {
@@ -58,7 +193,7 @@ namespace Opportunity.Converters
             DependencyProperty.Register("IfNeither", typeof(bool), typeof(ObjectToBooleanConverter), new PropertyMetadata(false));
 
         /// <summary>
-        /// Returns when <c>value == ValueForTrue &amp;&amp; value == ValueForFalse</c>.
+        /// Returns when <c>value</c> is in both <see cref="ValuesForTrue"/> and <see cref="ValuesForFalse"/>.
         /// </summary>
         public bool IfBoth
         {
@@ -72,38 +207,35 @@ namespace Opportunity.Converters
         public static readonly DependencyProperty IfBothProperty =
             DependencyProperty.Register("IfBoth", typeof(bool), typeof(ObjectToBooleanConverter), new PropertyMetadata(false));
 
-        private static void ValueChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            var s = (ObjectToBooleanConverter)d;
-            var tType = s.ValueForTrue?.GetType();
-            var fType = s.ValueForFalse?.GetType();
-            if (tType == null && fType == null)
-            {
-                s.valueType = typeof(object);
-                return;
-            }
-            if (tType == null)
-            {
-                s.valueType = fType;
-                return;
-            }
-            if (fType == null || tType == fType)
-            {
-                s.valueType = tType;
-                return;
-            }
-            // UNDONE: tType和fType的共同基类
-            s.valueType = typeof(object);
-        }
-
-        private Type valueType = typeof(object);
-
         /// <inheritdoc />
         protected override object ConvertImpl(object value, object parameter, string language)
         {
+            var comparer = this.ValueComparer ?? EqualityComparer<object>.Default;
             value = ChangeType(value, this.valueType);
-            var isTrue = Equals(value, this.ValueForTrue);
-            var isFalse = Equals(value, this.ValueForFalse);
+            var isTrue = false;
+            if(this.valuesForTrue!=null)
+            {
+                foreach (var item in this.valuesForTrue)
+                {
+                    if(comparer.Equals(value,item))
+                    {
+                        isTrue = true;
+                        break;
+                    }
+                }
+            }
+            var isFalse = false;
+            if (this.valuesForFalse != null)
+            {
+                foreach (var item in this.valuesForFalse)
+                {
+                    if (comparer.Equals(value, item))
+                    {
+                        isFalse = true;
+                        break;
+                    }
+                }
+            }
             if (isTrue && isFalse)
                 return this.IfBoth;
             if (isTrue)
@@ -118,9 +250,17 @@ namespace Opportunity.Converters
         {
             var v = ChangeType<bool>(value);
             if (v)
-                return this.ValueForTrue;
+            {
+                if (this.valuesForTrue == null || this.valuesForTrue.Count == 0)
+                    return null;
+                return this.valuesForTrue[0];
+            }
             else
-                return this.ValueForFalse;
+            {
+                if (this.valuesForFalse == null || this.valuesForFalse.Count == 0)
+                    return null;
+                return this.valuesForFalse[0];
+            }
         }
     }
 }
