4ce14bfb25b67a561b9a3caf03ad829355e0a078
OpportunityLiu <opportunity@live.in>
2018-04-29 13:05:59 -0300

    bug fix

--- Opportunity.Converters/Typed/+DateTime/DateTimeOffsetToStringConverter.cs
+++ Opportunity.Converters/Typed/+DateTime/DateTimeOffsetToStringConverter.cs
@@ -10,6 +10,10 @@ namespace Opportunity.Converters.Typed
         /// <inheritdoc />
         public override string Convert(DateTimeOffset value, object parameter, string language)
         {
+            if (value == default)
+            {
+                return "";
+            }
             return this.Formatter.Format(value);
         }
 
@@ -17,7 +21,9 @@ namespace Opportunity.Converters.Typed
         public override DateTimeOffset ConvertBack(string value, object parameter, string language)
         {
             if (string.IsNullOrWhiteSpace(value))
+            {
                 return default;
+            }
             return DateTimeOffset.Parse(value);
         }
     }
--- Opportunity.Converters/Typed/+DateTime/DateTimeToStringConverter.cs
+++ Opportunity.Converters/Typed/+DateTime/DateTimeToStringConverter.cs
@@ -10,6 +10,10 @@ namespace Opportunity.Converters.Typed
         /// <inheritdoc />
         public override string Convert(DateTime value, object parameter, string language)
         {
+            if (value == default)
+            {
+                return "";
+            }
             return this.Formatter.Format(value);
         }
 
@@ -17,7 +21,9 @@ namespace Opportunity.Converters.Typed
         public override DateTime ConvertBack(string value, object parameter, string language)
         {
             if (string.IsNullOrWhiteSpace(value))
+            {
                 return default;
+            }
             return DateTime.Parse(value);
         }
     }
