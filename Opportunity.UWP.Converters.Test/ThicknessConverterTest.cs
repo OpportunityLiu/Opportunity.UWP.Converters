f0531867d1c9d34b85590231946188c4ee711bf3
OpportunityLiu <opportunity@live.in>
2018-10-11 15:42:24 +0800

    thickness

---
+++ Opportunity.UWP.Converters.Test/ThicknessConverterTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using TC = Opportunity.UWP.Converters.XBind.Thickness;
+
+namespace Opportunity.UWP.Converters.Test
+{
+    [TestClass]
+    public class ThicknessConverterTest
+    {
+        [TestMethod]
+        public void Convert()
+        {
+            Assert.AreEqual(new Thickness(10, 30, 30, 50), TC.Convert(new Thickness(10, 20, 30, 40), " 0,  10 "));
+            Assert.AreEqual(new Thickness(0, 15, 40, 80), TC.Convert(new Thickness(10, 20, 30, 40), "-10,x0.5+5,10,*2"));
+        }
+
+        [TestMethod]
+        public void ConvertBack()
+        {
+            Assert.AreEqual(new Thickness(10, 20, 30, 40), TC.ConvertBack(new Thickness(10, 30, 30, 50), " 0,  10 "));
+            Assert.AreEqual(new Thickness(10, 20, 30, 40), TC.ConvertBack(new Thickness(0, 15, 40, 80), "-10,x0.5+5,10,*2"));
+        }
+
+    }
+}
